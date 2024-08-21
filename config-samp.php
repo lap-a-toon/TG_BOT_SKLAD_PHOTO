bdb4ee624564c9d3630a6c14f2e87850da7fc545
Xander Lexx <lap-a-toon@yandex.ru>
2024-08-21 15:12:35 +0500

    Настроил конфиг на обработку JSON-файла

--- config-samp.php
+++ config-samp.php
@@ -11,21 +11,20 @@ define('MAIN_PHOTO_FOLDER',__DIR__.'/YOUR_FINALLY_COLLECTING_FOLDER'); // Есл
 
 define('EXTENSIONS_ACCEPTED',['jpg','jpeg','png','bmp','heic','avi','mp4']);
 
-/**
- * Массив пользователей, заполнять вручную по шаблону
- * [
- *      'TG_USER_ID' => [
- *           'fio'=>'Фамилия_сотрудника',
- *           'sklad'=> 'База/Субсклад',
- *      ]
- * ]
- * где:
- *   TG_USER_ID - Телеграм ID, который пользователь получит в ответ на старт бота
- *   fio и sklad используются для именования каталогов при сохранении, соответственно, если использовать / в имени, то создастся подкаталог
- */
-$usersAccepted = [
-    'TG_USER_ID'=>[
-        'fio'=>'USER_LAST_NAME',
-        'sklad'=> 'ADDRESS_NAME/SUB_SKLAD',
-    ],
-];
+function getBotUsers(){
+    $json = file_get_contents(__DIR__.'/users.json'); 
+
+    // Check if the file was read successfully
+    if ($json === false) {
+        die('Error reading the JSON file');
+    }
+
+    // Decode the JSON file
+    $json_data = json_decode($json, true); 
+
+    // Check if the JSON was decoded successfully
+    if ($json_data === null) {
+        die('Error decoding the JSON file');
+    }
+    return $json_data;
+}
